2984e0c9559bc65c7fcbe9c679b01b3c10f9c9aa
youyinnn <youyinnn@gmail.com>
2020-03-22 13:58:02 +0800

    refine metadata updater regex logic

--- src/plugins/metadata-updater.js
+++ src/plugins/metadata-updater.js
@@ -5,21 +5,21 @@ function update(mdFilePath, data) {
     let originalText = fs.readFileSync(mdFilePath, { encoding: 'utf-8'})
     let newText = originalText
     if (data.newArticleTitle !== undefined) {
-        newText = newText.replace(/title: ?.*[\n|\r|\r\n]/, `title: ${data.newArticleTitle}\r`)
+        newText = newText.replace(/title:\s*.*\s*[\n|\r|\r\n]/, `title: ${data.newArticleTitle}\r`)
     }
     if (data.newArticleCates !== undefined) {
         let newArticleCatesText = 'categories:\r'
         data.newArticleCates.forEach((c) => {
             newArticleCatesText += `  - ${c}\r`
         })
-        newText = newText.replace(/categories:\s*[\n|\r|\r\n](\s\s\-\s*.*[\n|\r|\r\n])*/, newArticleCatesText)
+        newText = newText.replace(/categories:\s*[\n|\r|\r\n](\s\s\-\s.*\s*[\n|\r|\r\n])*/, newArticleCatesText)
     }
     if (data.newArticleTags !== undefined) {
         let newArticleTagsText = 'tags:\r'
         data.newArticleTags.forEach((c) => {
             newArticleTagsText += `  - ${c}\r`
         })
-        newText = newText.replace(/tags:\s*[\n|\r|\r\n](\s\s\-\s*.*[\n|\r|\r\n])*/, newArticleTagsText)
+        newText = newText.replace(/tags:\s*[\n|\r|\r\n](\s\s\-\s.*\s*[\n|\r|\r\n])*/, newArticleTagsText)
     }
     fs.writeFileSync(mdFilePath, newText, { encoding: 'utf-8'})
     if (data.newArticleTitle !== undefined) {
